d803364f2417aab0aaff3b871a9c2a91f702dd55
jiyeong <jiyeongstar@gmail.com>
2021-11-23 13:13:25 +0900

    initial setting

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import Cover from '../Pages/Cover'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cover from "Pages/Cover";
+import Home from "Pages/Home";
+import Career from "Pages/Career";
+import Stack from "Pages/Stack";
+import Project from "Pages/Project";
+import Contact from "Pages/Contact";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Cover />}></Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Cover />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/career" element={<Career />}></Route>
+        <Route path="/stack" element={<Stack />}></Route>
+        <Route path="/project" element={<Project />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
