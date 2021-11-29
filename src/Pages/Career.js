0c282efe5ab42563e18c2729c5db131f3a344372
Kim, GiBeom <gibum1228@gmail.com>
2021-11-29 19:00:30 +0900

    개발중

--- src/Pages/Career.js
+++ src/Pages/Career.js
@@ -1,9 +1,20 @@
+import SimpleCard from 'Components/SimpleCard'
 import React from 'react'
 
 const Career = () => {
     return (
         <div>
-            Career Pages
+            <h1>활동 이력</h1>
+            <div className="section">
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+                <SimpleCard name="테스트1" term="2021.11.10. ~ 2021.12.05" text="심플 카드 컴포넌트 테스트"/>
+            </div>
         </div>
     )
 }
