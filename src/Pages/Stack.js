2f626e4c067cdff4359ca9c2c579ac739f770e01
Kim, GiBeom <gibum1228@gmail.com>
2021-11-29 18:37:05 +0900

    기술 스택 페이지 및 기술 카드 컴포넌트 완성 아이콘은 json에서 url를 불러와 삽입함

--- src/Pages/Stack.js
+++ src/Pages/Stack.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import StackCard from 'Components/StackCard'
+import icon from 'Json/icon.json'
+import 'Css/Stack.scss'
 
 const Stack = () => {
     return (
         <div>
-            Stack pages
+            <h1>기술 스택 페이지</h1>
+            <div className="section">
+                <StackCard src={icon.react}/>
+                <StackCard src={icon.spring}/>
+                <StackCard src={icon.tf}/>
+                <StackCard src={icon.keras}/>
+                <StackCard src={icon.pytorch}/>
+                <StackCard src={icon.git}/>
+                <StackCard src={icon.git}/>
+                <StackCard src={icon.git}/>
+            </div>
         </div>
     )
 }
