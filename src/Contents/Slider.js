f74dcf2f20d687ecf4943481d6d0c029bbe0f7c5
jiyeong <jiyeongstar@gmail.com>
2021-12-06 18:55:47 +0900

    #3 스크롤다운

--- src/Contents/Slider.js
+++ src/Contents/Slider.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const Slider = ({ children }) => {
   const [active, setActive] = useState(0);
+  const [pageNum, setPageNum] = useState();
+  const [direction, setDirection] = useState("");
   const goPrev = () => {
     if (active >= 0) {
       setActive((prev) => prev - 1);
@@ -11,22 +13,35 @@ const Slider = ({ children }) => {
     if (active < children.length - 1) {
       setActive((prev) => prev + 1);
     }
+    console.log(active);
+  };
+  const toTop = () => {
+    setActive(0);
   };
   const percentage = -100 * active + "%";
-  console.log(children.length);
-  const handleScroll = (e) => {
-    // console.log(e.deltaY);
-    // e.preventDefault();
-    // e.deltaY > 0 ? goNext() : goPrev();
-    const scrollY = window.scrollY;
-    console.log(scrollY);
-  };
-  console.log(children);
+
+  let timer;
+
+  window.addEventListener("wheel", (e) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      if (e.deltaY > 0) {
+        //scroll wheel down
+        if (direction !== "down") {
+          console.log("down");
+          setDirection("down");
+
+          goNext();
+        }
+      }
+    }, 500);
+  });
 
   return (
     <div>
       <div
-        onScroll={handleScroll}
         className="slides"
         style={{ transform: `translate3d(0, ${percentage}, 0)` }}
       >
@@ -46,6 +61,7 @@ const Slider = ({ children }) => {
         <button onClick={goNext} disabled={active === children.length - 1}>
           Next Slide &darr;
         </button>
+        {active > 0 && <button onClick={toTop}>맨 위로 올라가기</button>}
       </div>
       <ul className="dots">
         {children.map((child, index) => (
