3313fc44ea75ba9c5595d63ecd67c308862c0101
Kim, GiBeom <gibum1228@gmail.com>
2022-02-05 14:45:03 +0900

    StackCard 컴포넌트를 json으로 효율적으로 호출

--- src/Components/StackCard.js
+++ src/Components/StackCard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "Css/StackCard.scss";
+import icon from "Json/icon.json";
 
 function StackCard({ src }) {
-  return <img src={src} className="stackCard" />;
+  const img = String(src);
+  return <img src={icon[img]} className="stackCard" />;
 }
 
 export default StackCard;
--- src/Pages/Stack.js
+++ src/Pages/Stack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import StackCard from "Components/StackCard";
-import icon from "Json/icon.json";
+import stack from "Json/stack.json";
 import "Css/Stack.scss";
 
 const Stack = () => {
@@ -8,14 +8,11 @@ const Stack = () => {
     <div className="component stack">
       <h1>기술 스택 페이지</h1>
       <div className="stack__section">
-        <StackCard src={icon.react} />
-        <StackCard src={icon.spring} />
-        <StackCard src={icon.tf} />
-        <StackCard src={icon.keras} />
-        <StackCard src={icon.pytorch} />
-        <StackCard src={icon.git} />
-        <StackCard src={icon.git} />
-        <StackCard src={icon.git} />
+        {stack.map((data) => (
+          <>
+            <StackCard src={data.src} />
+          </>
+        ))}
       </div>
     </div>
   );
