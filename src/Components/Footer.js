1c881111c0e6891818da8073a9d803049af04f2c
Kim, GiBeom <gibum1228@gmail.com>
2022-02-18 04:39:36 +0900

    #9 Footer 물결 효과 추가

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "Css/footer.scss";
+
+function Footer() {
+  return (
+    <body>
+      <footer>
+        <div class="waves">
+          <div class="wave" id="wave1"></div>
+          <div class="wave" id="wave2"></div>
+          <div class="wave" id="wave3"></div>
+          <div class="wave" id="wave4"></div>
+        </div>
+      </footer>
+    </body>
+  );
+}
+
+export default Footer;
