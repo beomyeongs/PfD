82df6858dcacfc6570f21f977ddbacaaca57deb8
jiyeong <jiyeongstar@gmail.com>
2022-02-19 14:42:10 +0900

    #12 Cover페이지 수정

--- src/Pages/Cover.js
+++ src/Pages/Cover.js
@@ -1,8 +1,32 @@
 // cover home career stack project contact
 import React from "react";
-
+import "../Css/Cover.scss";
+import image from "../image/bg1.jpg";
 const Cover = () => {
-  return <div className="component cover">Cover Pages</div>;
+  return (
+    <div className="cover">
+      <div className="cover__left">
+        <div className="cover__left__wrapper">
+          <h2 className="cover__intro">Hello, My Name Is</h2>
+          <h1 className="cover__name">JIYEONG PARK</h1>
+          <div className="cover__title">
+            <div className="cover__title__wrapper">
+              <div className="cover__title__item">1.Web Developer</div>
+              <div className="cover__title__item">2.Web Developer</div>
+              <div className="cover__title__item">3.Web Developer</div>
+            </div>
+          </div>
+          <p className="cover__desc">
+            개발자를 꿈꾸며 <br /> 프론트엔드 개발자를 시작으로 풀스택 개발자가
+            되는 것이 목표입니다.
+          </p>
+        </div>
+      </div>
+      <div className="cover__right">
+        <img src={image} alt="" className="cover__img" />
+      </div>
+    </div>
+  );
 };
 
 export default Cover;
