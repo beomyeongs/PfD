f355b4349b25654ce4cc76904fd4d8981562e37f
Kim, GiBeom <gibum1228@gmail.com>
2022-03-01 01:14:35 +0900

    Contact 페이지 꾸미기

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,56 @@
 import React from "react";
 import "Css/Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPerson,
+  faEnvelope,
+  faPhone,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <body>
-      <footer>
-        <div class="waves">
-          <div class="wave" id="wave1"></div>
-          <div class="wave" id="wave2"></div>
-          <div class="wave" id="wave3"></div>
-          <div class="wave" id="wave4"></div>
+      <section class="contact">
+        <div class="content">
+          <h1>
+            Contact Me
+            <FontAwesomeIcon icon={faThumbsUp} />
+          </h1>
+          <p>저에 대해 더 궁금하시면 아래 내용을 보고 연락주세요.</p>
         </div>
-      </footer>
+        <div class="container">
+          <div class="contactInfo">
+            <div class="box">
+              <div class="icon">
+                <FontAwesomeIcon icon={faPerson} />
+              </div>
+              <div class="text">
+                <h3>Info.</h3>
+                <p>김기범, 24</p>
+              </div>
+            </div>
+            <div class="box">
+              <div class="icon">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </div>
+              <div class="text">
+                <h3>Email.</h3>
+                <p>example@gmail.com</p>
+              </div>
+            </div>
+            <div class="box">
+              <div class="icon">
+                <FontAwesomeIcon icon={faPhone} />
+              </div>
+              <div class="text">
+                <h3>Phone.</h3>
+                <p>010-XXXX-XXXX</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </body>
   );
 }
