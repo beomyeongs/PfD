1b0d46cd3b633a6aba07271627b025d54f553c6a
Kim, GiBeom <gibum1228@gmail.com>
2022-03-03 08:22:26 +0900

    #17 기술 스택 횟수를 정리한 그래프 그리기

--- src/Pages/Stack.js
+++ src/Pages/Stack.js
@@ -1,18 +1,12 @@
 import React from "react";
-import StackCard from "Components/StackCard";
-import icon from "Json/icon.json";
-import stack from "Json/stack";
 import "Css/Stack.scss";
+import TechGraph from "Components/TechGraph";
 
 const Stack = () => {
   return (
-    <div className="component stack">
+    <div>
       <h1>기술 스택 페이지</h1>
-      <div className="stack__section">
-        {stack.map((data) => (
-          <StackCard src={data} />
-        ))}
-      </div>
+      <TechGraph />
     </div>
   );
 };
